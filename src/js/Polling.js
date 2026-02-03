1d38738983bb01294ee93f50e83161344ca4eed2
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2026-02-03 20:04:42 +0300

    api

--- src/js/Polling.js
+++ src/js/Polling.js
@@ -1,92 +1,105 @@
 import { ajax } from 'rxjs/ajax';
-import { map, catchError, of } from 'rxjs';
-import { interval } from 'rxjs';
+import { interval, map, catchError, of } from 'rxjs';
 import { startWith, switchMap } from 'rxjs/operators';
 
 import '../css/Polling.css';
 
 export default class Polling {
-  constructor(root) {
+  constructor(root, apiUrl = 'http://localhost:3000/messages/unread') {
     this.root = root;
+    this.apiUrl = apiUrl;
 
-    this.messagesElement = null;
     this.messages = [];
-    this.cards = [];
-
-    this.apiUrl = 'http://localhost:3000/messages/unread';
+    this.polling$ = null;
   }
 
   init() {
     this.render();
-
-    this.getFromApi().subscribe((data) => {
-      this.messages = data;
-      this.render();
-    });
+    this.startPolling();
   }
 
-  getFromApi() {
+  startPolling() {
+    this.polling$ = interval(5000)
+      .pipe(
+        startWith(0),
+        switchMap(() => this.fetchMessages()),
+      )
+      .subscribe({
+        next: (messages) => this.updateMessages(messages),
+        error: (err) => console.error('Error: ', err),
+      });
+  }
+
+  fetchMessages() {
     return ajax.getJSON(this.apiUrl).pipe(
-      map((data) => data.messages || []),
+      map((data) => data.messages),
       catchError((error) => {
-        console.error('error: ', error);
+        console.error('Request failed', error);
         return of([]);
       }),
     );
   }
 
-  startPolling() {
-    this.polling$ = interval(5000).pipe(
-      startWith(0),
-      switchMap(() => this.getFromApi())
-    ).subscribe(messages => {
-      this.messages = messages;
-      this.render();
-    });
+  updateMessages(newMessages) {
+    if (this.messages !== newMessages) {
+      this.messages = newMessages;
+      this.renderMessages();
+    }
   }
 
   render() {
     if (!this.root) return;
     this.root.innerHTML = '';
 
-    const pollingContainer = document.createElement('div');
-    pollingContainer.className = 'message-container';
+    const messageContainer = document.createElement('div');
+    messageContainer.className = 'message-container';
 
-    const pollingTitle = document.createElement('h2');
-    pollingTitle.className = 'message-title';
-    pollingTitle.textContent = 'Incoming';
+    const titleElement = document.createElement('h2');
+    titleElement.className = 'message-title';
+    titleElement.textContent = 'Incoming';
 
-    this.messagesElement = document.createElement('ul');
-    this.messagesElement.className = 'messages-list';
+    this.messagesList = document.createElement('ul');
+    this.messagesList.className = 'messages-list';
 
-    this.renderCard();
-    this.messagesElement.append(...this.cards);
+    messageContainer.append(titleElement, this.messagesList);
+    this.root.append(messageContainer);
 
-    pollingContainer.append(pollingTitle, this.messagesElement);
-
-    this.root.append(pollingContainer);
+    this.renderMessages();
   }
 
-  renderCard() {
+  renderMessages() {
+    if (!this.messagesList) return;
+    this.messagesList.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+
     this.messages.forEach((message) => {
-      const card = document.createElement('li');
-      card.className = 'message-card';
-
-      const cardFrom = document.createElement('div');
-      cardFrom.className = 'message-card-from';
-
-      const cardSubj = document.createElement('div');
-      cardSubj.className = 'message-card-subj';
-
-      const cardRecieved = document.createElement('div');
-      cardRecieved.className = 'message-card-recieved';
-
-      cardFrom.textContent = message.from;
-      cardSubj.textContent = message.subject;
-      cardRecieved.textContent = message.recieved;
-
-      card.append(cardFrom, cardSubj, cardRecieved);
-      this.cards.push(card);
+      const card = this.createMessageCard(message);
+      fragment.append(card);
     });
+
+    this.messagesList.append(fragment);
+  }
+
+  createMessageCard(message) {
+    const card = document.createElement('li');
+    card.className = 'message-card';
+
+    const cardFrom = document.createElement('div');
+    cardFrom.className = 'message-card-from';
+
+    const cardSubj = document.createElement('div');
+    cardSubj.className = 'message-card-subj';
+
+    const cardRecieved = document.createElement('div');
+    cardRecieved.className = 'message-card-recieved';
+
+    cardFrom.textContent = message.from;
+    cardSubj.textContent = message.subject;
+    cardRecieved.textContent = message.recieved;
+
+    card.append(cardFrom, cardSubj, cardRecieved);
+
+    return card;
   }
 }
